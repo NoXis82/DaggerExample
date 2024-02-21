61775bbf7e9b7f7e4aa1d4075fac3e0f3a3eaa62
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-02-21 20:08:04 +0300

    add application component

---
+++ app/src/main/java/com/noxis/daggerexample/di/AppComponent.kt
@@ -0,0 +1,20 @@
+package com.noxis.daggerexample.di
+
+import android.app.Application
+import com.noxis.daggerexample.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+
+@Component(modules = [AndroidSupportInjectionModule::class])
+interface AppComponent : AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(app: Application): Builder
+        fun build(): AppComponent
+    }
+
+}
