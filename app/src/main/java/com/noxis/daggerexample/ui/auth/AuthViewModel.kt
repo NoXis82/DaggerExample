00f5d9e995b09648a3c139fc1deea6f898c28f46
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-02-26 22:18:48 +0300

    add livedata

--- app/src/main/java/com/noxis/daggerexample/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/noxis/daggerexample/ui/auth/AuthViewModel.kt
@@ -1,34 +1,28 @@
 package com.noxis.daggerexample.ui.auth
 
 import android.annotation.SuppressLint
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
+import com.noxis.daggerexample.models.User
 import com.noxis.daggerexample.repository.auth.AuthApi
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 @SuppressLint("CheckResult")
-class AuthViewModel @Inject constructor(authApi: AuthApi) : ViewModel() {
+class AuthViewModel @Inject constructor(private val authApi: AuthApi) : ViewModel() {
 
-    init {
-        Log.d(TAG, "AuthViewModel init")
-        authApi.getFakeStuff(1)
-            .toObservable()
+    private val authUser = MediatorLiveData<User>()
+    fun authenticateWithId(userId: Int) {
+        authApi.getFakeStuff(userId)
             .subscribeOn(Schedulers.io())
+            .toObservable()
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                {
-                    Log.d(TAG, "onNext: $it")
-                },
-                {
-                    Log.e(TAG, "onError: ", it)
-                },
-                {
-                    Log.i(TAG, "onComplete")
-                }
-            )
+            .subscribe { authUser.value = it }
     }
+
+    fun observerUser(): LiveData<User> = authUser
 
     companion object {
         private const val TAG = "AuthViewModel"
