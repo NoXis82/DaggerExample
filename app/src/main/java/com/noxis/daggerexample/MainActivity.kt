27729abd9d09e5ab9986af459f1a76fff8e86368
Vycheslav Tumanov <tumanov@umarsh.ru>
2024-03-04 23:08:54 +0300

    add menu logout

--- app/src/main/java/com/noxis/daggerexample/MainActivity.kt
+++ app/src/main/java/com/noxis/daggerexample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.noxis.daggerexample
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 
 class MainActivity : BaseActivity() {
@@ -8,6 +10,21 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(applicationContext, TAG, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+                sessionManager.logOut()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
